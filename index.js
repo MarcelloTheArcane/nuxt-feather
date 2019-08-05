46a72826479482323bd51ed2f3c68e580fcf0bd8
Max Reynolds <max@tillysinteriors.com>
2019-08-05 12:18:53 +0100

    Don't need to camel case

--- index.js
+++ index.js
@@ -8,22 +8,8 @@ export default {
 
     requireComponent.keys().forEach(fileName => {
       const componentConfig = requireComponent(fileName)
-      const componentName = camelCase(fileName.replace(/(^\.\/|\.\w+$)/g, ''))
+      const componentName = fileName.replace(/(^\.\/|\.\w+$)/g, '')
       Vue.component(componentName, componentConfig.default || componentConfig)
     })
   }
-}
-
-function upperFirst(string) {
-  return string[0].toLocaleUpperCase() + string.slice(1)
-}
-
-function camelCase(string) {
-  return string
-    .toLocaleLowerCase()
-    .split(/[ _-]+/g)
-    .reduce((final, section) => {
-      final += upperFirst(section)
-      return final
-    }, '')
 }
